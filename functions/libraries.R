94d43e4c761fc40a780ad91d81c467ff4efe7354
Nahom Debela <nahom.debela@wsu.edu>
2020-09-20 22:07:04 -0700

    common functions

--- functions/libraries.R
+++ functions/libraries.R
@@ -1,1 +1,7 @@
-library(lubridate)
+library(lubridate)   # used for arranging dates
+library(matrixStats) # used for rowMeans()
+library(rvest);	     # imbd web scraping
+library(stringr);	   
+library(pryr);       # mem_used()
+library(lmtest);     # linear regression
+library(MASS);       # polr
